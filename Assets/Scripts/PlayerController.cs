da80c45e664e5aac6802b8b5b3d1e1e8d54b875a
Oscar Luis Muñoz Martínez <oscarmunozdev@gmail.com>
2021-02-14 19:40:54 +0100

    -- moving main character

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 2.5f;
+
+    private Rigidbody2D _rigidbody;
+    private Animator _animator;
+
+    private Vector2 _movement;
+    private bool _facingRight = true;
+
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        _movement = new Vector2(horizontalInput, 0f);
+
+        if (horizontalInput < 0f && _facingRight == true)
+        {
+            Flip();
+        } else if (horizontalInput > 0f && _facingRight == false)
+        {
+            Flip();
+        }
+
+    }
+
+    private void FixedUpdate()
+    {
+        float horizontalVelocity = _movement.normalized.x * speed;
+        _rigidbody.velocity = new Vector2(horizontalVelocity, _rigidbody.velocity.y);
+    }
+
+    private void LateUpdate()
+    {
+        _animator.SetBool("Idle", _movement == Vector2.zero);
+    }
+
+    private void Flip()
+    {
+        _facingRight = !_facingRight;
+        float localScaleX = transform.localScale.x;
+        localScaleX = localScaleX * -1f;
+        transform.localScale = new Vector3(localScaleX, transform.localScale.y, transform.localScale.z);
+    }
+}
